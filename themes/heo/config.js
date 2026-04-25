f9bf0df8f4e3a7f10baa6ab754b6cdd28ae517b5
Travis0234 <travisbreaker34@outlook.com>
2026-04-25 21:42:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: '/' },
-    { title: 'Welcome to Travis's Blog', url: '/' }
+    { title: 'Welcome to My Blog', url: '/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
